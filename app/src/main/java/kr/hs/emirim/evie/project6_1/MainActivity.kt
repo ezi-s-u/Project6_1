75b9126d58696d49b2d1e2b340e3bb4a2526d9a9
ezi-s-u <ljsgoo0617@gmail.com>
2023-06-27 19:34:57 +0900

    Project 6-1 완성

--- app/src/main/java/kr/hs/emirim/evie/project6_1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/project6_1/MainActivity.kt
@@ -1,11 +1,14 @@
 package kr.hs.emirim.evie.project6_1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Chronometer
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.TimePicker
@@ -16,25 +19,58 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnStart : Button
     lateinit var btnDone : Button
     lateinit var rg : RadioGroup
+    lateinit var radioCal : RadioButton
+    lateinit var radioTime : RadioButton
     lateinit var calendar : CalendarView
-    lateinit var timePicker : TimePicker
+    lateinit var timePick : TimePicker
     lateinit var textResult : TextView
     var selectedYear : Int = 0
     var selectedMonth : Int = 0
-    var selcetedDay : Int = 0
+    var selectedDay : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         chrono1 = findViewById(R.id.chrono1)
         btnStart = findViewById(R.id.btnStart)
         btnDone = findViewById(R.id.btnDone)
+        radioCal = findViewById(R.id.radioDate)
+        radioTime = findViewById(R.id.radioTime)
         rg = findViewById(R.id.rg)
         calendar = findViewById(R.id.calendar)
-        timePicker = findViewById(R.id.timePicker)
+        timePick = findViewById(R.id.timePicker)
         textResult = findViewById(R.id.textResult)
 
-        timePicker.visibility = View.INVISIBLE
+        timePick.visibility = View.INVISIBLE
         calendar.visibility = View.INVISIBLE
 
+        radioCal.setOnClickListener {
+            timePick.visibility = View.INVISIBLE
+            calendar.visibility = View.VISIBLE
+        }
+
+        radioTime.setOnClickListener {
+            timePick.visibility = View.VISIBLE
+            calendar.visibility = View.INVISIBLE
+        }
+
+        btnStart.setOnClickListener {
+            chrono1.base = SystemClock.elapsedRealtime()
+            chrono1.start()
+            chrono1.setTextColor(Color.RED)
+        }
+        
+        btnDone.setOnClickListener { 
+            chrono1.stop()
+            chrono1.setTextColor(Color.BLUE)
+            textResult.setText(""+ selectedYear + "년 " + selectedMonth +"월 "+selectedDay+"일 ") //맨 앞에 정수가 오면 error
+            textResult.append("" + timePick.currentHour + "시 " + timePick.currentMinute + "분 ")
+            textResult.append("예약됨")
+        }
+        
+        calendar.setOnDateChangeListener { calendarView, year, month, day -> // 맨 앞에 받는 객체를 쓴다
+            selectedYear = year
+            selectedMonth = month
+            selectedDay = day
+        }
     }
 }
